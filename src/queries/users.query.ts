d06e171a4c235ebf16ed1e7bc8444019fd9fe5da
Samuel Adebayo <sambayo28@gmail.com>
2022-03-20 15:42:05 +0100

    update dev nodemon for restart

--- src/queries/users.query.ts
+++ src/queries/users.query.ts
@@ -1,12 +1,16 @@
 import { postgresClient } from "@database";
 import { User } from "@interfaces";
 
+/** fetch users from database */
 export const fetchAllUsers = async (): Promise<User[]> => {
   const { rows: users } = await postgresClient.query<User>(`Select * from users`);
   return users;
 }
 
+/** fetch single user from database using email */
 export const fetchSingleUser = async (email: string): Promise<User> => {
   const data = await postgresClient.query<User>(`SELECT * FROM users WHERE email = '${email}'`);
   return data.rows[0];
 }
+export const namas = [];
+
